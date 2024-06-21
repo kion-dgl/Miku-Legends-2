19d1b2473e609914cd7e3936b38065b04a12211d
kion <kion@dashgl.com>
2024-06-21 16:22:10 +0900

    more shoes

---
+++ tests/shoes-asbestos.test.ts
@@ -0,0 +1,57 @@
+import { test, expect } from "bun:test";
+import { readFileSync } from "fs";
+import ByteReader from '../src/ByteReader';
+import shoesAsbestos from '../fixtures/shoes-asbestos.json'
+
+type MeshHeader = {
+    name: string;
+    triCount: number;
+    quadCount: number;
+    vertCount: number;
+    triOfs: number;
+    quadOfs: number;
+    vertOfs: number;
+    triShadowOfs: number;
+    quadShadowOfs: number;
+}
+
+test("Reading the strip offsets for the shoes", () => {
+
+    const buffer = readFileSync(`./bin/PL00P002.BIN`);
+    const dat = buffer.subarray(0x30, 0x30 + 0x2b40);
+    const reader = new ByteReader(dat.buffer as ArrayBuffer);
+
+    const FEET_OFS = 0x1800
+    const names = ["20_ASBESTOS_RIGHT_FOOT", "21_ASBESTOS_LEFT_FOOT"];
+    reader.seek(FEET_OFS);
+
+    const meshes: MeshHeader[] = [];
+    names.forEach((name) => {
+
+        const triCount = reader.readUInt8();
+        const quadCount = reader.readUInt8();
+        const vertCount = reader.readUInt8();
+        reader.seekRel(1);
+
+        const triOfs = reader.readUInt32();
+        const quadOfs = reader.readUInt32();
+        const vertOfs = reader.readUInt32();
+        const triShadowOfs = reader.readUInt32();
+        const quadShadowOfs = reader.readUInt32();
+
+        meshes.push({
+            name,
+            triCount,
+            quadCount,
+            vertCount,
+            triOfs,
+            quadOfs,
+            vertOfs,
+            triShadowOfs,
+            quadShadowOfs
+        })
+    });
+
+    expect(meshes).toEqual(shoesAsbestos);
+
+});
