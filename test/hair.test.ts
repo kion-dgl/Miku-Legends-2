81aa75940396b35d4c896ce0ca0d1ddaad6fdb8e
kion <kion@dashgl.com>
2024-07-10 02:00:22 +0900

    fix broken hair test

--- test/hair.test.ts
+++ test/hair.test.ts
@@ -2,6 +2,13 @@ import { test, expect } from "bun:test";
 import { readFileSync } from "fs";
 import ByteReader from '../src/ByteReader';
 import hair from '../fixtures/hair.json';
+import { Vector3, Matrix4 } from "three";
+import { encodeVertexBits } from "../src/MeshWriter";
+import { dwordString } from "../src/MeshReader";
+const SCALE = 0.00125;
+const RESTORE = 800;
+const ROT = new Matrix4();
+ROT.makeRotationX(Math.PI);
 
 type MeshHeader = {
     name: string;
@@ -17,9 +24,10 @@ type MeshHeader = {
 
 test("Reading the strip offsets for the hair", () => {
 
-    const buffer = readFileSync(`./bin/PL00P001.BIN`);
-    const dat = buffer.subarray(0x30, 0x30 + 0x2b40);
-    const reader = new ByteReader(dat.buffer as ArrayBuffer);
+    const file = readFileSync(`./bin/PL00P001.BIN`);
+    const dat = file.subarray(0x30, 0x30 + 0x2b40);
+    const { buffer } = Buffer.from(dat);
+    const reader = new ByteReader(buffer as ArrayBuffer);
 
     const HEAD_OFS = 0xb60
     const names = ["10_HAIR", "11_FACE", "12_MOUTH"];
@@ -53,5 +61,5 @@ test("Reading the strip offsets for the hair", () => {
     });
 
     expect(meshes).toEqual(hair);
-
 });
+
