992e997e89d40805065df406be811d984695fb49
kion <kion@dashgl.com>
2024-07-11 17:31:15 +0900

    add obj files

---
+++ index.ts
@@ -0,0 +1,28 @@
+import { doReplace } from './src/Encode';
+
+doReplace(
+    "Miku/miku_body.png",
+    "Miku/miku_face.png",
+    // Body Encoding
+    "Miku/02_BODY.obj",
+    "Miku/03_HIPS.obj",
+    "Miku/10_LEG_RIGHT_TOP.obj",
+    "Miku/11_LEG_RIGHT_BOTTOM.obj",
+    "Miku/13_LEG_LEFT_TOP.obj",
+    "Miku/14_LEG_LEFT_BOTTOM.obj",
+    // Feet
+    "Miku/12_RIGHT_FOOT.obj",
+    "Miku/15_LEFT_FOOT.obj",
+    // Left Arm
+    "Miku/07_LEFT_SHOULDER.obj",
+    "Miku/08_LEFT_ARM.obj",
+    "Miku/09_LEFT_HAND.obj",
+    // Right Arm
+    "Miku/04_RIGHT_SHOULDER.obj",
+    "Miku/05_RIGHT_ARM.obj",
+    "Miku/06_RIGHT_HAND.obj",
+    // Head
+    "Miku/01_HEAD_HAIR.obj",
+    "Miku/01_HEAD_FACE.obj",
+    "Miku/01_HEAD_MOUTH.obj"
+)
