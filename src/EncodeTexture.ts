af638748225761e727f44383e54d98e3c32caed3
kion <kion@dashgl.com>
2024-07-16 10:56:14 +0900

    compress and encode texture

--- src/EncodeTexture.ts
+++ src/EncodeTexture.ts
@@ -10,61 +10,53 @@ const encodeTexel = (r: number, g: number, b: number, a: number) => {
   return texel;
 };
 
+const readPixel = (data: Buffer, inOfs: number, pal: number[]) => {
+  const a = data.readUInt8(inOfs + 3) === 0 ? 0 : 255;
+  const r = a === 0 ? 0 : data.readUInt8(inOfs + 0);
+  const g = a === 0 ? 0 : data.readUInt8(inOfs + 1);
+  const b = a === 0 ? 0 : data.readUInt8(inOfs + 2);
+  const texel = encodeTexel(r, g, b, a);
+
+  // Search through the existing palette
+  const index = pal.indexOf(texel);
+
+  // If doesn't exist, we add it to the palette
+  if (index === -1) {
+    const pos = pal.length;
+    pal.push(texel);
+    return pos;
+  }
+
+  return index;
+};
+
 const encodeImage = (pngSrc: Buffer) => {
   const pngInfo = PNG.sync.read(pngSrc);
   const { width, height, data } = pngInfo;
-
-  console.log("Encoding image");
 
   if (width !== 256 || height !== 256) {
     throw new Error("Encoder expects a 256x256 image");
   }
 
-  console.log(data.length.toString(16));
-
   let inOfs = 0;
   let outOfs = 0;
   const palette: number[] = [0];
-  const pal = Buffer.alloc(0x20, 0);
+  const pal = Buffer.alloc(0x80, 0);
   const img = Buffer.alloc(0x8000, 0);
-
-  const readPixel = () => {
-    const a = data.readUInt8(inOfs + 3) === 0 ? 0 : 255;
-    const r = a === 0 ? 0 : data.readUInt8(inOfs + 0);
-    const g = a === 0 ? 0 : data.readUInt8(inOfs + 1);
-    const b = a === 0 ? 0 : data.readUInt8(inOfs + 2);
-    const texel = encodeTexel(r, g, b, a);
-    inOfs += 4;
-
-    // Search through the existing palette
-    const index = palette.indexOf(texel);
-
-    // If doesn't exist, we add it to the palette
-    if (index === -1) {
-      const pos = palette.length;
-      palette.push(texel);
-      return pos;
-    }
-
-    return index;
-  };
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x += 2) {
-      const lowByte = readPixel();
-      const highByte = readPixel();
+      const lowByte = readPixel(data, inOfs, palette);
+      inOfs += 4;
+      const highByte = readPixel(data, inOfs, palette);
+      inOfs += 4;
       const byte = ((highByte << 4) | lowByte) & 0xff;
       img[outOfs] = byte;
       outOfs++;
     }
   }
 
-  // for(let i = 0; i < palette.length; i++){
-  //     console.log(palette[i].toString(16))
-  // }
-
   outOfs = 0;
-  console.log("Palette colors: ");
   for (let i = 0; i < 16; i++) {
     const texel = palette[i] || 0x0000;
     console.log(texel.toString(16));
@@ -75,62 +67,182 @@ const encodeImage = (pngSrc: Buffer) => {
   return [pal, img];
 };
 
-const replaceTexture = (
-  gamefile: Buffer,
-  bodyBuffer: Buffer,
-  faceBuffer: Buffer,
-) => {
-  const modded = Buffer.from(gamefile);
-  const [bodyPal, bodyImg] = encodeImage(bodyBuffer);
-  const [facePal, faceImg] = encodeImage(faceBuffer);
+const compressSegment = (segment: Buffer): [boolean[], Buffer] => {
+  // Create a boolean array and out buffer
+  const bucket: boolean[] = [];
+  const compressed = Buffer.alloc(segment.length);
 
-  let ofs = 0;
+  // Number of min and max number of words to match
+  const MAX_CAP = 9;
+  const MIN_CAP = 4;
 
-  // Paltte 0
-  const bodyPalOfs = 0x30;
-  ofs = bodyPalOfs;
-  for (let i = 0; i < bodyPal.length; i++) {
-    modded[bodyPalOfs + i] = bodyPal[i];
-  }
+  let inOfs = 0;
+  let outOfs = 0;
 
-  // Image 0
-  const bodyImgOfs = 0x800;
-  for (let i = 0; i < 0x8000; i++) {
-    modded[bodyImgOfs + i] = bodyImg[i];
-  }
+  do {
+    // Check ahead
+    let found = false;
+    const wordsLeft = (segment.length - inOfs) / 2;
+    const maxCount = wordsLeft < MAX_CAP ? wordsLeft : MAX_CAP;
 
-  // palette 1
-  // Black for the second texture
-  ofs = 0x8830;
-  for (let pal = 0; pal < 3; pal++) {
-    for (let i = 0; i < 16; i++) {
-      modded.writeUInt16LE(0x8000, ofs);
-      ofs += 2;
+    // Check ahead
+
+    for (let count = maxCount; count >= MIN_CAP; count--) {
+      const len = count * 2;
+      const needle = segment.subarray(inOfs, inOfs + len);
+      const window = segment.subarray(0, inOfs);
+      const needleOfs = window.indexOf(needle);
+
+      if (needleOfs === -1) {
+        continue;
+      }
+
+      found = true;
+      const lowBits = count - 2;
+      const highBits = needleOfs << 3;
+      const word = highBits | lowBits;
+      compressed.writeUInt16LE(word, outOfs);
+      bucket.push(true);
+      outOfs += 2;
+      inOfs += len;
+      break;
     }
+
+    if (!found) {
+      const word = segment.readUInt16LE(inOfs);
+      inOfs += 2;
+      bucket.push(false);
+      compressed.writeUInt16LE(word, outOfs);
+      outOfs += 2;
+    }
+  } while (inOfs < segment.length);
+
+  // Write a true bit and 0xffff to finish the segment
+  bucket.push(true);
+  compressed.writeUInt16LE(0xffff, outOfs);
+  outOfs += 2;
+
+  const payload = compressed.subarray(0, outOfs);
+  return [bucket, payload];
+};
+
+const encodeBitfield = (bits: boolean[]): Buffer => {
+  const length = Math.ceil(bits.length / 32) * 4;
+  let ofs = 0;
+  const buffer = Buffer.alloc(length);
+  const dword = new Uint32Array(1);
+
+  for (let i = 0; i < bits.length; i += 32) {
+    dword[0] = 0;
+    bits[i + 0] && (dword[0] |= 0x80000000);
+    bits[i + 1] && (dword[0] |= 0x40000000);
+    bits[i + 2] && (dword[0] |= 0x20000000);
+    bits[i + 3] && (dword[0] |= 0x10000000);
+
+    bits[i + 4] && (dword[0] |= 0x8000000);
+    bits[i + 5] && (dword[0] |= 0x4000000);
+    bits[i + 6] && (dword[0] |= 0x2000000);
+    bits[i + 7] && (dword[0] |= 0x1000000);
+
+    bits[i + 8] && (dword[0] |= 0x800000);
+    bits[i + 9] && (dword[0] |= 0x400000);
+    bits[i + 10] && (dword[0] |= 0x200000);
+    bits[i + 11] && (dword[0] |= 0x100000);
+
+    bits[i + 12] && (dword[0] |= 0x80000);
+    bits[i + 13] && (dword[0] |= 0x40000);
+    bits[i + 14] && (dword[0] |= 0x20000);
+    bits[i + 15] && (dword[0] |= 0x10000);
+
+    bits[i + 16] && (dword[0] |= 0x8000);
+    bits[i + 17] && (dword[0] |= 0x4000);
+    bits[i + 18] && (dword[0] |= 0x2000);
+    bits[i + 19] && (dword[0] |= 0x1000);
+
+    bits[i + 20] && (dword[0] |= 0x800);
+    bits[i + 21] && (dword[0] |= 0x400);
+    bits[i + 22] && (dword[0] |= 0x200);
+    bits[i + 23] && (dword[0] |= 0x100);
+
+    bits[i + 24] && (dword[0] |= 0x80);
+    bits[i + 25] && (dword[0] |= 0x40);
+    bits[i + 26] && (dword[0] |= 0x20);
+    bits[i + 27] && (dword[0] |= 0x10);
+
+    bits[i + 28] && (dword[0] |= 0x8);
+    bits[i + 29] && (dword[0] |= 0x4);
+    bits[i + 30] && (dword[0] |= 0x2);
+    bits[i + 31] && (dword[0] |= 0x1);
+
+    buffer.writeUInt32LE(dword[0], ofs);
+    ofs += 4;
   }
 
-  // Pallete 2
-  const facePalOfs = 0x9030;
-  for (let i = 0; i < facePal.length; i++) {
-    modded[facePalOfs + i] = facePal[i];
+  return buffer;
+};
+
+const compressTexture = (pal: Buffer, img: Buffer) => {
+  const decompressed = Buffer.concat([pal, img]);
+
+  const segments = [
+    decompressed.subarray(0x0000, 0x2000),
+    decompressed.subarray(0x2000, 0x4000),
+    decompressed.subarray(0x4000, 0x6000),
+    decompressed.subarray(0x6000, 0x8000),
+    decompressed.subarray(0x8000),
+  ];
+
+  const bits: boolean[] = [];
+  const loads: Buffer[] = [];
+  segments.forEach((segment, index) => {
+    const [b, p] = compressSegment(segment);
+    b.forEach((bit) => bits.push(bit));
+    loads.push(p);
+  });
+
+  const bitfied = encodeBitfield(bits);
+  return [bitfied, Buffer.concat(loads)];
+};
+
+const replaceTexture = (gamefile: Buffer, bodyBuffer: Buffer) => {
+  const modded = Buffer.from(gamefile);
+
+  // Replace Body
+  const [bodyPal, bodyImg] = encodeImage(bodyBuffer);
+  const [bodyBitField, compressedBody] = compressTexture(bodyPal, bodyImg);
+
+  // First we zero out the previous image
+  for (let i = 0; i < 0x3000; i++) {
+    modded[i] = 0;
   }
 
-  // Image 1
-  const faceImgOfs = 0x9800;
-  for (let i = 0; i < 0x4000; i++) {
-    modded[faceImgOfs + i] = faceImg[i];
+  modded.writeInt16LE(bodyBitField.length, 0x24);
+  let bodyOfs = 0x30;
+  for (let i = 0; i < bodyBitField.length; i++) {
+    modded[bodyOfs++] = bodyBitField[i];
+  }
+
+  for (let i = 0; i < compressedBody.length; i++) {
+    modded[bodyOfs++] = compressedBody[i];
+  }
+
+  // Replace Body Alternate Palette
+  const BODY_ALT_PAL_OFS = 0x3030;
+  for (let i = 0; i < bodyPal.length; i++) {
+    modded[BODY_ALT_PAL_OFS + i] = bodyPal[i];
   }
 
   return modded;
 };
 
-const encodeTexture = (bodyTexture: string, faceTexture: string) => {
+const encodeTexture = (bodyTexture: string) => {
   // Encode the body and face texture to write to ROM
   const srcTexture = readFileSync("bin/PL00T.BIN");
+
+  // Read the body Image
   const bodyBuffer = readFileSync(bodyTexture);
-  const faceBuffer = readFileSync(faceTexture);
-  const modTexture = replaceTexture(srcTexture, bodyBuffer, faceBuffer);
-  writeFileSync("mod/PL00T.BIN", modTexture);
+  const modTexture = replaceTexture(srcTexture, bodyBuffer);
+  writeFileSync("out/PL00T.BIN", modTexture);
 };
 
 export { encodeTexture };
