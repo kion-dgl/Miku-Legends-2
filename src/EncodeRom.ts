7ad2431ac0886b13a73503ca86043e127503a66e
kion <kion@dashgl.com>
2024-07-20 07:58:50 +0900

    update player table

--- src/EncodeRom.ts
+++ src/EncodeRom.ts
@@ -111,8 +111,12 @@ const findPointerTable = (rom: Buffer) => {
 
 const updatePointerTable = (rom: Buffer) => {
   const ofs = findPointerTable(rom);
-  const dword = rom.readUint32LE(ofs);
-  rom.writeUInt32LE(dword, ofs + 4);
+  const PLAYER_OFFSET = 0x110800;
+  rom.writeUInt32LE(0x110 + PLAYER_OFFSET, ofs + 4); // Head
+  rom.writeUInt32LE(0x158 + PLAYER_OFFSET, ofs + 24); // Feet
+  rom.writeUInt32LE(0x188 + PLAYER_OFFSET, ofs + 16); // Left Arm
+  rom.writeUInt32LE(0x1d0 + PLAYER_OFFSET, ofs + 20); // Buster
+  rom.writeUInt32LE(0x218 + PLAYER_OFFSET, ofs + 12); // Right Arm
 };
 
 const replaceInRom = (
