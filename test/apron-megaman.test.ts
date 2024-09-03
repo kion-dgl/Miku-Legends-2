a5e6baeb633676d7ad4e2f825b7593fdabb800bb
kion <kion@dashgl.com>
2024-09-03 17:28:51 +0900

    local vertices for megaman apron

--- test/apron-megaman.test.ts
+++ test/apron-megaman.test.ts
@@ -24,6 +24,7 @@ import { readFileSync, writeFileSync } from "fs";
 import ByteReader from "../src/ByteReader";
 
 import { Bone, Vector3, Matrix4 } from "three";
+import { readVertexList, readFace } from "../src/MeshReader";
 
 type EntityHeader = {
   id: string;
@@ -40,9 +41,20 @@ type HierarchyStruct = {
   shareVertices: boolean;
 };
 
+type TextureCoords = {
+  imageX: number;
+  imageY: number;
+  paletteX: number;
+  paletteY: number;
+};
+
 const SCALE = 0.00125;
 const ROT = new Matrix4();
 ROT.makeRotationX(Math.PI);
+
+const VERTEX_MASK = 0b1111111111;
+const VERTEX_MSB = 0b1000000000;
+const VERTEX_LOW = 0b0111111111;
 
 test("it should create a obj of apron megaman", () => {
   const file = readFileSync("bin/ST0305.BIN");
@@ -124,6 +136,50 @@ test("it should create a obj of apron megaman", () => {
     bones.push(bone);
   }
 
+  // ** Texture ** //
+  const mats: TextureCoords[] = [];
+  if (textureOfs) {
+    reader.seek(textureOfs);
+    const textureCount = ((collisionOfs || shadowOfs) - textureOfs) / 4;
+    for (let i = 0; i < textureCount; i++) {
+      const imageCoords = reader.readUInt16();
+      const paletteCoords = reader.readUInt16();
+      const imageX = (imageCoords & 0x0f) << 6;
+      const imageY = imageCoords & 0x10 ? 0x100 : 0;
+
+      const paletteX = (paletteCoords & 0x3f) << 4;
+      const paletteY = paletteCoords >> 6;
+      mats.push({ imageX, imageY, paletteX, paletteY });
+    }
+  }
+
+  expect(mats).toEqual([
+    {
+      imageX: 320,
+      imageY: 0,
+      paletteX: 0,
+      paletteY: 241,
+    },
+    {
+      imageX: 320,
+      imageY: 0,
+      paletteX: 64,
+      paletteY: 241,
+    },
+    {
+      imageX: 384,
+      imageY: 0,
+      paletteX: 128,
+      paletteY: 241,
+    },
+    {
+      imageX: 320,
+      imageY: 0,
+      paletteX: 192,
+      paletteY: 241,
+    },
+  ]);
+
   // ** Heirarchy ** //
 
   const hierarchy: HierarchyStruct[] = [];
@@ -158,4 +214,46 @@ test("it should create a obj of apron megaman", () => {
     bone.updateMatrix();
     bone.updateMatrixWorld();
   });
+
+  // ** Read Geomtry ** //
+
+  for (let i = 0; i < submeshCount; i++) {
+    // Read triangle offset and count
+    reader.seek(geometryOfs + i * 0x10);
+    const triCount = reader.readUInt8();
+    const quadCount = reader.readUInt8();
+    const vertCount = reader.readUInt8();
+    const scaleBytes = reader.readUInt8();
+    const triOfs = reader.readUInt32();
+    const quadOfs = reader.readUInt32();
+    const vertOfs = reader.readUInt32();
+
+    // Read Scale
+    const scale = scaleBytes === -1 ? 0.5 : 1 << scaleBytes;
+    const { boneIndex, boneParent, shareVertices } = hierarchy[i];
+    const bone = bones[boneIndex];
+
+    // Read Vertices
+    const vertices = readVertexList(reader, vertOfs, vertCount);
+    const tris = readFace(reader, triOfs, triCount, false);
+    const quads = readFace(reader, quadOfs, quadCount, true);
+
+    const obj: string[] = [];
+    vertices.forEach(({ x, y, z }) => {
+      obj.push(`v ${x.toFixed(3)} ${y.toFixed(3)} ${z.toFixed(3)}`);
+    });
+
+    tris.forEach((face) => {
+      const [a, b, c] = face;
+      obj.push(`f ${a.index + 1} ${b.index + 1} ${c.index + 1}`);
+    });
+
+    quads.forEach((face) => {
+      const [a, b, c, d] = face;
+      obj.push(`f ${a.index + 1} ${b.index + 1} ${d.index + 1} ${c.index + 1}`);
+    });
+
+    const name = `mesh_${i.toString().padStart(3, "0")}`;
+    writeFileSync(`./fixtures/APRON/${name}.OBJ`, obj.join("\n"));
+  }
 });
