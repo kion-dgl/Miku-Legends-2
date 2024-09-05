90463279715e3db8fc4874d687d771fda3105d23
kion <kion@dashgl.com>
2024-09-05 18:43:40 +0900

    jumbled textures

--- test/apron-megaman.test.ts
+++ test/apron-megaman.test.ts
@@ -48,13 +48,247 @@ type TextureCoords = {
   paletteY: number;
 };
 
-const SCALE = 0.00125;
-const ROT = new Matrix4();
-ROT.makeRotationX(Math.PI);
+import { PNG } from "pngjs";
 
-const VERTEX_MASK = 0b1111111111;
-const VERTEX_MSB = 0b1000000000;
-const VERTEX_LOW = 0b0111111111;
+type Pixel = {
+  r: number;
+  g: number;
+  b: number;
+  a: number;
+};
+
+const wordToColor = (word: number): Pixel => {
+  const r = ((word >> 0x00) & 0x1f) << 3;
+  const g = ((word >> 0x05) & 0x1f) << 3;
+  const b = ((word >> 0x0a) & 0x1f) << 3;
+  const a = word > 0 ? 255 : 0;
+  return { r, g, b, a };
+};
+
+const renderTexture = (src: Buffer, outName: string) => {
+  const tim = {
+    type: src.readUInt32LE(0x00),
+    fullSize: src.readUInt32LE(0x04),
+    paletteX: src.readUInt16LE(0x0c),
+    paletteY: src.readUInt16LE(0x0e),
+    colorCount: src.readUInt16LE(0x10),
+    paletteCount: src.readUInt16LE(0x12),
+    imageX: src.readUInt16LE(0x14),
+    imageY: src.readUInt16LE(0x16),
+    width: src.readUInt16LE(0x18),
+    height: src.readUInt16LE(0x1a),
+    bitfieldSize: src.readUInt16LE(0x24),
+    payloadSize: src.readUInt16LE(0x26),
+  };
+
+  console.log(tim);
+
+  switch (tim.colorCount) {
+    case 16:
+      tim.width *= 4;
+      break;
+    case 256:
+      tim.width *= 2;
+      break;
+    default:
+      tim.paletteCount *= tim.colorCount / 16;
+      tim.colorCount = 16;
+      tim.width *= 4;
+      break;
+  }
+
+  const { fullSize, bitfieldSize } = tim;
+  const bitfield: number[] = new Array();
+  const target = Buffer.alloc(fullSize);
+
+  // Read Bitfield
+
+  const bitfieldBuffer = src.subarray(0x30, 0x30 + bitfieldSize);
+  let ofs = 0x30;
+  for (let i = 0; i < bitfieldSize; i += 4) {
+    const dword = src.readUInt32LE(ofs + i);
+    for (let k = 31; k > -1; k--) {
+      bitfield.push(dword & (1 << k) ? 1 : 0);
+    }
+  }
+
+  ofs += bitfieldSize;
+  const payloadStart = 0;
+
+  // Decompress
+
+  let outOfs = 0;
+  let windowOfs = 0;
+  let cmdCount = 0;
+  let bytes = 0;
+
+  for (let i = 0; i < bitfield.length; i++) {
+    const bit = bitfield[i];
+    if (outOfs === fullSize) {
+      const payload = src.subarray(0x30 + bitfieldSize, ofs);
+      break;
+    }
+
+    const word = src.readUInt16LE(ofs);
+    ofs += 2;
+
+    switch (bit) {
+      case 0:
+        target.writeUInt16LE(word, outOfs);
+        outOfs += 2;
+        break;
+      case 1:
+        if (word === 0xffff) {
+          windowOfs += 0x2000;
+          cmdCount = 0;
+          bytes = 0;
+        } else {
+          cmdCount++;
+          const copyFrom = windowOfs + ((word >> 3) & 0x1fff);
+          const copyLen = ((word & 0x07) + 2) * 2;
+          bytes += copyLen;
+          for (let i = 0; i < copyLen; i++) {
+            target[outOfs++] = target[copyFrom + i];
+          }
+        }
+        break;
+    }
+  }
+
+  ofs = 0;
+  const { colorCount, paletteCount } = tim;
+  const palette: Pixel[][] = new Array();
+  for (let i = 0; i < paletteCount; i++) {
+    palette[i] = new Array();
+    for (let k = 0; k < colorCount; k++) {
+      const word = target.readUInt16LE(ofs);
+      ofs += 2;
+      palette[i].push(wordToColor(word));
+    }
+  }
+
+  // Read the image data
+  const imageData: number[] = new Array();
+  for (ofs; ofs < target.length; ofs++) {
+    const byte = target.readUInt8(ofs);
+    if (colorCount === 256) {
+      imageData.push(byte);
+    } else {
+      imageData.push(byte & 0xf);
+      imageData.push(byte >> 4);
+    }
+  }
+
+  const { width, height } = tim;
+  const png = new PNG({ width, height });
+
+  let index = 0;
+  let dst = 0;
+  for (let y = 0; y < height; y++) {
+    for (var x = 0; x < width; x++) {
+      const colorIndex = imageData[index++];
+      const { r, g, b, a } = palette[0][colorIndex!];
+      png.data[dst++] = r;
+      png.data[dst++] = g;
+      png.data[dst++] = b;
+      png.data[dst++] = a;
+    }
+  }
+
+  // Export file
+  const buffer = PNG.sync.write(png);
+  writeFileSync(`fixtures/APRON/${outName}.png`, buffer);
+  return imageData;
+};
+
+// The first thing we need to do is iterate through the files and export all of the textures
+// once we've located the spectific megaman textures, then we will update the test to export
+// the specific one and write a fixture for it
+
+test("it should dump all of the textures, from the texture files", () => {
+  const INPUT = ["cut-ST03T.BIN"];
+
+  console.log("----================= Yeeeallo =================----");
+
+  let img: number[] = [];
+  INPUT.forEach((filename) => {
+    const file = readFileSync(`bin/${filename}`);
+    for (let i = 0x43000; i <= 0x46000; i += 0x800) {
+      const type = file.readUInt32LE(i + 0x00);
+      const fullSize = file.readUInt32LE(i + 0x04);
+      const colorCount = file.readUInt16LE(i + 0x10);
+      const paletteCount = file.readUInt16LE(i + 0x12);
+      const width = file.readUInt16LE(i + 0x18);
+      const height = file.readUInt16LE(i + 0x1a);
+
+      if (fullSize % 4 !== 0) {
+        continue;
+      }
+
+      if (fullSize < 0x20) {
+        continue;
+      }
+
+      const name = `${filename}-${i.toString(16).padStart(6, "0")}-true`;
+
+      if (type === 0x02) {
+        console.log("Palette found");
+
+        let ofs = i + 0x30;
+        const palette: Pixel[][] = new Array();
+        for (let i = 0; i < paletteCount; i++) {
+          palette[i] = new Array();
+          for (let k = 0; k < colorCount; k++) {
+            const word = file.readUInt16LE(ofs);
+            ofs += 2;
+            palette[i].push(wordToColor(word));
+          }
+        }
+
+        const png = new PNG({ width: 256, height: 256 });
+
+        let index = 0;
+        let dst = 0;
+        for (let y = 0; y < 256; y++) {
+          for (var x = 0; x < 256; x++) {
+            const colorIndex = img[index++];
+            const { r, g, b, a } = palette[0][colorIndex!];
+
+            if (y < 1) {
+              console.log(r, g, b, a);
+            }
+            png.data[dst++] = r;
+            png.data[dst++] = g;
+            png.data[dst++] = b;
+            png.data[dst++] = a;
+          }
+        }
+
+        // Export file
+        const buffer = PNG.sync.write(png);
+        writeFileSync(`fixtures/APRON/${name}.png`, buffer);
+      }
+
+      if (width === 0 || height === 0) {
+        continue;
+      }
+
+      if (type !== 0x03) {
+        continue;
+      }
+
+      console.log(`Compressed texture 0x${i.toString(16).padStart(6, "0")}`);
+      // if (colorCount === 0) {
+      //   console.log("no palette, skipping");
+      //   continue;
+      // }
+      const src = file.subarray(i);
+
+      const p = renderTexture(src, name);
+      img = img.length == 0 ? p : img;
+    }
+  });
+});
 
 test("it should create a obj of apron megaman", () => {
   const file = readFileSync("bin/ST0305.BIN");
@@ -118,6 +352,10 @@ test("it should create a obj of apron megaman", () => {
 
   const nbBones = Math.floor((heirarchyOfs - skeletonOfs) / 6);
   reader.seek(skeletonOfs);
+  const SCALE = 0.00125;
+  const ROT = new Matrix4();
+  ROT.makeRotationX(Math.PI);
+
   for (let i = 0; i < nbBones; i++) {
     // Read Bone Position
     const x = reader.readInt16();
@@ -130,6 +368,8 @@ test("it should create a obj of apron megaman", () => {
     const vec3 = new Vector3(x, y, z);
     vec3.multiplyScalar(SCALE);
     vec3.applyMatrix4(ROT);
+
+    console.log(i, vec3.y);
     bone.position.x = vec3.x;
     bone.position.y = vec3.y;
     bone.position.z = vec3.z;
