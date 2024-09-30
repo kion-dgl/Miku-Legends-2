54a2df5eea4803ebaf635de35a4683cb8248c786
kion <kion@dashgl.com>
2024-09-30 17:57:37 +0900

    lofi roll in barrel room

--- index.ts
+++ index.ts
@@ -220,9 +220,11 @@ encodeApronMegaman();
 updateST03T("miku/apron/body-01.png", "miku/faces/ST03T.png");
 updateSceneModel();
 updateDemoLogo("miku/title-smol.png");
+
 updateFlutterPaintings(
   "miku/paintings/megaman-room.png",
   "miku/paintings/roll-room.png",
+  "miku/paintings/roll-lofi.png",
 );
 updateYosyonkePaintings(
   "miku/paintings/room-203.png",
