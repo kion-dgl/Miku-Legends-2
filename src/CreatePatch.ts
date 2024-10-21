0b3ec9e0bba29d7a36d68427d23cb8c00e83e28b
kion <kion@dashgl.com>
2024-10-21 12:56:14 +0900

    create patch

---
+++ src/CreatePatch.ts
@@ -0,0 +1,56 @@
+import { readFileSync, writeFileSync } from "fs";
+
+type GamePatch = {
+  offset: number;
+  bytes: Buffer;
+};
+
+const createPatch = () => {
+  const sourceRom = process.env.SRC_ROM;
+  if (!sourceRom) {
+    throw new Error("Need to set SRC_ROM value in .env");
+  }
+
+  const romDst = process.env.DST_ROM;
+  if (!romDst) {
+    throw new Error("Need to set DST_ROM value in .env");
+  }
+
+  const rom = readFileSync(sourceRom);
+  const mod = readFileSync(romDst);
+
+  // The sequence should be an array where the offsets that are
+  // different between the source and the mod are added to the sequence
+
+  const patch = [];
+
+  let isDiff = false;
+  let start = 0;
+  let len = 0;
+
+  console.log("Create Patch");
+  for (let i = 0; i < rom.length; i++) {
+    if (!isDiff && mod[i] !== rom[i]) {
+      // First we want to start logging when the bytes are different
+      start = i;
+      len = 1;
+      isDiff = true;
+    } else if (isDiff && mod[i] !== rom[i]) {
+      // And if the bytes are different, we want to record how many
+      len++;
+    } else if (isDiff && mod[i] === rom[i]) {
+      // Only once the bytes stop being different do we log them into the patch
+      patch.push({
+        offset: i,
+        bytes: mod.subarray(start, start + len),
+      });
+      isDiff = false;
+      len = 0;
+    }
+  }
+
+  writeFileSync("./docs/patch.json", JSON.stringify(patch, null, 2));
+};
+
+export default createPatch;
+export { createPatch };
